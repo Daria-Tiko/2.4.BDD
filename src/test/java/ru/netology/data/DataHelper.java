ec25b7f0762f9179caf87c27889fc345710fbed4
Darya Tishchenko QA-41 <tdaniel@bk.ru>
2023-02-12 18:12:12 +0400

    Init commit

---
+++ src/test/java/ru/netology/data/DataHelper.java
@@ -0,0 +1,52 @@
+package ru.netology.data;
+
+import lombok.Value;
+
+
+import java.util.Random;
+
+public class DataHelper {
+    private DataHelper() {
+    }
+
+    public static AuthInfo getAuthInfo() {
+
+        return new AuthInfo("vasya", "qwerty123");
+    }
+
+    @Value
+    public static class AuthInfo {
+        String login;
+        String password;
+    }
+
+    public static VerificationCode getVerificationCode(AuthInfo authInfo) {
+
+        return new VerificationCode("12345");
+    }
+
+    @Value
+    public static class VerificationCode {
+        String code;
+    }
+
+    public static CardInfo getFirstCard() {
+        return new CardInfo("5559000000000002", "0f3f5c2a-249e-4c3d-8287-09f7a039391d");
+    }
+
+    public static CardInfo getSecondCard() {
+        return new CardInfo ("5559000000000002","0f3f5c2a-249e-4c3d-8287-09f7a039391d");
+    }
+
+    public static int generateValidAmount (int balance) {
+        return new Random().nextInt(balance)+1;
+    }
+    public static int generateInvalidAmount (int balance) {
+        return Math.abs(balance) + new Random().nextInt(10000);
+    }
+    @Value
+    public static class CardInfo {
+        String cardNumber;
+        String testId;
+    }
+}
