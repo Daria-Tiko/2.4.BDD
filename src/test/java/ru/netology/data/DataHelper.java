7f867a57580c5e4d8bcc767c8e8a8bfbddb58c64
Darya Tishchenko QA-41 <tdaniel@bk.ru>
2023-02-13 15:24:32 +0400

    Add page/test

--- src/test/java/ru/netology/data/DataHelper.java
+++ src/test/java/ru/netology/data/DataHelper.java
@@ -14,13 +14,13 @@ public class DataHelper {
         return new AuthInfo("vasya", "qwerty123");
     }
 
-    @Value
+   @Value
     public static class AuthInfo {
-        String login;
-        String password;
+        private String login;
+        private String password;
     }
 
-    public static VerificationCode getVerificationCode(AuthInfo authInfo) {
+    public static VerificationCode getVerificationCode() {
 
         return new VerificationCode("12345");
     }
@@ -30,18 +30,18 @@ public class DataHelper {
         String code;
     }
 
-    public static CardInfo getFirstCard() {
+    public static CardInfo getFirstCardInfo() {
+        return new CardInfo("5559000000000001", "92df3f1c-a033-48e6-8390-206f6b1f56c0");
+    }
+
+    public static CardInfo getSecondCardInfo() {
         return new CardInfo("5559000000000002", "0f3f5c2a-249e-4c3d-8287-09f7a039391d");
     }
 
-    public static CardInfo getSecondCard() {
-        return new CardInfo ("5559000000000002","0f3f5c2a-249e-4c3d-8287-09f7a039391d");
+    public static int generateValidAmount(int balance) {
+        return new Random().nextInt(balance) + 1;
     }
-
-    public static int generateValidAmount (int balance) {
-        return new Random().nextInt(balance)+1;
-    }
-    public static int generateInvalidAmount (int balance) {
+    public static int generateInvalidAmount(int balance) {
         return Math.abs(balance) + new Random().nextInt(10000);
     }
     @Value
